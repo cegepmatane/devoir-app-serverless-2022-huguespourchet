d606ea0c970b14fef339194195880d7bf8f6f686
MoOaAaa <71814380+MoOaAaa@users.noreply.github.com>
2022-01-31 11:31:03 -0500

    App electron

---
+++ app/Application.js
@@ -0,0 +1,41 @@
+﻿class Application {
+  constructor(window, vueListeCadeau, vueCadeau, cadeauDAO){
+
+    this.window = window;
+
+    this.vueListeCadeau = vueListeCadeau;
+
+    this.vueCadeau = vueCadeau;
+
+    this.cadeauDAO = cadeauDAO;
+
+    // C'est l'équivalent de function(){this.naviguer()}
+    this.window.addEventListener("hashchange", () =>this.naviguer());
+
+    this.naviguer();
+  }
+
+  naviguer(){
+    let hash = window.location.hash;
+
+    if(!hash){
+
+      this.vueListeCadeau.initialiserListeCadeau(this.cadeauDAO.lister());
+      this.vueListeCadeau.afficher();
+
+    }else{
+
+      let navigation = hash.match(/^#cadeau\/([0-9]+)/);
+      let idCadeau = navigation[1];
+
+      let cadeau = this.cadeauDAO.chercher(parseInt(idCadeau))
+      this.vueCadeau.initialiserCadeau(cadeau);
+      this.vueCadeau.afficher();
+
+    }
+  }
+
+}
+
+new Application(window, new VueListeCadeau(), new VueCadeau(), new CadeauDAO());
+
